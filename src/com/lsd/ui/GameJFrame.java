e3ef54213b34efe88cdc9f78ffaabde4dfd265ae
lsd <2814503436@qq.com>
2025-06-06 00:24:12 +0800

    查看完整图片，作弊码，判断胜利

--- src/com/lsd/ui/GameJFrame.java
+++ src/com/lsd/ui/GameJFrame.java
@@ -4,12 +4,21 @@ import javax.swing.*;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
-public class GameJFrame extends JFrame {
+public class GameJFrame extends JFrame implements KeyListener {
 
     //二维数组,管理数据，加载图片需用到，扩大范围
     int[][] data =new int[4][4];
+    //记录空白的二维数组
+    int x =0;
+    int y =0;
+    //路径
+    String path ="image\\animal\\animal3\\";
+
+    int[][] win = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}};
 
     public GameJFrame() {
         //初始化界面
@@ -36,13 +45,21 @@ public class GameJFrame extends JFrame {
             tempArray[i] = tempArray[index];
             tempArray[index] = temp;
         }
-
-        int index =0;
+//old
+/*        int index =0;
         //赋值二维数组
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data[i].length; j++) {
                 data[i][j] = tempArray[index];//或index=0，index++，，， tempArray[i*4+j]
                 index++;
+            }
+        }*/
+        for (int i = 0; i < tempArray.length; i++) {
+            if(tempArray[i]==0){
+                x=i/4;
+                y=i%4;
+            }else {
+                data[i/4][i%4]=tempArray[i];
             }
         }
     }
@@ -58,8 +75,14 @@ public class GameJFrame extends JFrame {
 //        //获取隐藏容器panel再添加
 //        this.getContentPane().add(Jlabel);
 
+        //清空已出现
+        this.getContentPane().removeAll();
+        if(victory()) {
+            JLabel winpic = new JLabel(new ImageIcon("image\\win.png"));
+            winpic.setBounds(203,283,197,73);
+            this.getContentPane().add(winpic);
 
-
+        }
         //int num =1; //old
         //外循环，把内循环重复4次
         for (int i = 0; i < 4; i++) {
@@ -67,7 +90,7 @@ public class GameJFrame extends JFrame {
             for (int j = 0; j < 4; j++) {
                 int num = data[i][j];
                 //创建一个label对象，管理容器加图片
-                JLabel Jlabel = new JLabel(new ImageIcon("image\\animal\\animal3\\"+num+".jpg"));
+                JLabel Jlabel = new JLabel(new ImageIcon(path+num+".jpg"));
                 Jlabel.setBounds(105*j+83, 105*i+134, 105, 105);//偏移中间
                 //设置图片的边框
                 //Jlabel.setBorder(new LineBorder(Color.yellow));
@@ -86,6 +109,9 @@ public class GameJFrame extends JFrame {
         background.setBounds(40,40,508,560);
         //隐藏容器添加JLabel 对象
         this.getContentPane().add(background);
+
+        //刷新一下
+        this.getContentPane().repaint();
     }
 
     private void initJFrame() {
@@ -105,6 +131,9 @@ public class GameJFrame extends JFrame {
 
         //取消默认的组件居中方式，细节
         this.setLayout(null);
+
+        //给整个界面添加键盘监听事件
+        this.addKeyListener(this);
     }
     private void initJMenuBar() {
         //初始化菜单栏
@@ -133,4 +162,100 @@ public class GameJFrame extends JFrame {
         //test2
 
     }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    //按下不松开
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        if (keyCode == 65) {
+            //清空已出现
+            this.getContentPane().removeAll();
+
+            JLabel allPic = new JLabel(new ImageIcon(path+"all.jpg"));
+            allPic.setBounds(83,134,420,420);
+            //allPic.setBounds(40,40,508,560);
+            this.getContentPane().add(allPic);
+
+            //背景，先图片，后背景图D:\java-project\test-pintu\image\background.png
+            JLabel background = new JLabel(new ImageIcon("image\\background.png"));
+            background.setBounds(40,40,508,560);
+            //隐藏容器添加JLabel 对象
+            this.getContentPane().add(background);
+            //刷新一下
+            this.getContentPane().repaint();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(victory())
+            return;//结束
+
+        //左37，上38，右39，下40
+        int code = e.getKeyCode();
+        System.out.println(code);
+        if(code==39){
+            if(y==0){
+                return;
+            }
+            System.out.println("右移动");
+            data[x][y] =data[x][y-1];
+            data[x][y-1] =0;
+            y--;
+            initImage();
+        }else if(code==38){
+            if(x==3){
+                return;
+            }
+            System.out.println("图片向上移动");
+            data[x][y] =data[x+1][y];
+            data[x+1][y] =0;
+            x++;
+            initImage();
+        }else if(code==37){
+            if(y==3){
+                return;
+            }
+            System.out.println("左移动");
+            data[x][y] =data[x][y+1];
+            data[x][y+1] =0;
+            y++;
+            initImage();
+        }else if(code==40){
+            if(x==0){
+                return;
+            }
+            System.out.println("下移动");
+            data[x][y] =data[x-1][y];
+            data[x-1][y] =0;
+            x--;
+            initImage();
+        }else if(code==65){
+            initImage();
+        }else if(code==87){
+            data = new int[][]
+                    {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}};
+            initImage();
+            //bug,多个空格
+
+        }
+
+    }
+
+    public boolean victory(){
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                if(data[i][j]!=win[i][j]){
+                    return false;//只要有一个不一样
+                }
+            }
+        }
+        return true;//完毕
+    }
+
 }
