cba85818801a34325f72e3a2bd2fb7a1e3b69e68
lsd <2814503436@qq.com>
2025-06-05 15:13:33 +0800

    1，主界面添加图片，取消隐藏容器居中，细节，可让存放图片的容器，根据x，y布局图片 next：打乱图片，二维数字

--- src/com/lsd/ui/GameJFrame.java
+++ src/com/lsd/ui/GameJFrame.java
@@ -9,10 +9,40 @@ public class GameJFrame extends JFrame {
         initJFrame();
         //初始化菜单
         initJMenuBar();
+        //图片
+        initImage();
         //可见
         this.setVisible(true);
         //shift+ enter 中途换行
     }
+
+    private void initImage() {
+//        //成就ImageIcon对象
+//        //ImageIcon icon = new ImageIcon("D:\\java-project\\test-pintu\\image\\animal\\animal3\\1.jpg");
+//
+//        //创建一个label对象，管理容器
+//        JLabel Jlabel = new JLabel(new ImageIcon("D:\\java-project\\test-pintu\\image\\animal\\animal3\\1.jpg"));
+//        Jlabel.setBounds(0, 0, 105, 105);//105,105
+//        //this.add(Jlabel);
+//        //获取隐藏容器panel再添加
+//        this.getContentPane().add(Jlabel);
+        int num =1;
+        //外循环，把内循环重复4次
+        for (int i = 0; i < 4; i++) {
+            //内循环，添加4张图片
+            for (int j = 0; j < 4; j++) {
+                //创建一个label对象，管理容器加图片
+                JLabel Jlabel = new JLabel(new ImageIcon("D:\\java-project\\test-pintu\\image\\animal\\animal3\\"+num+".jpg"));
+                Jlabel.setBounds(105*j, 105*i, 105, 105);//105,105
+                //获取隐藏容器panel再添加
+                this.getContentPane().add(Jlabel);
+                //加载下一张
+                num++;
+            }
+        }
+
+    }
+
     private void initJFrame() {
         //标题
         this.setTitle("拼图单机版v1.0.0");
@@ -20,7 +50,7 @@ public class GameJFrame extends JFrame {
         this.setSize(488,430);
         //多个窗口，在顶
         this.setAlwaysOnTop(true);
-        //居中
+        //居中,窗口显示居中
         this.setLocationRelativeTo(null);
         //默认关闭，0,1,2什么也不做，暴露,关闭，，JFrame 实现了 WindowConstants 接口
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,7 +58,8 @@ public class GameJFrame extends JFrame {
         //this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         //this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);//仅销毁当前窗口，但程序继续运行（常用于子窗口）
 
-
+        //取消默认的组件居中方式，细节
+        this.setLayout(null);
     }
     private void initJMenuBar() {
         //初始化菜单栏
